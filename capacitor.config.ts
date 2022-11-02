eed3406addcb6e7d90ce788add3e55bff10d04a9
welsonoktario <welsonoktario@yahoo.com>
2022-11-02 12:45:04 +0700

    Update depedencies

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,18 @@
-import { CapacitorConfig } from "@capacitor/cli";
+import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: "com.eangkot.com",
-  appName: "eAngkot",
-  webDir: "dist",
+  appId: 'com.eangkot.com',
+  appName: 'eAngkot',
+  webDir: 'dist',
   bundledWebRuntime: false,
   server: {
     cleartext: true,
   },
-};
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
+}
 
-export default config;
+export default config
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,92 +1,92 @@
-import { Http, HttpOptions } from "@capacitor-community/http";
-import { Preferences } from "@capacitor/preferences";
-import { keysToSnake } from "./bodySerializer";
+import { CapacitorHttp as Http, HttpOptions } from '@capacitor/core'
+import { Preferences } from '@capacitor/preferences'
+import { keysToSnake } from './bodySerializer'
 
-const API_URL = process.env.VUE_APP_API_URL;
+const API_URL = process.env.VUE_APP_API_URL
 
 export const get = async (endpoint: string) => {
-  const isAPI = !endpoint.includes("http");
-  const url = isAPI ? API_URL + endpoint : endpoint;
-  let headers: { Authorization: string };
+  const isAPI = !endpoint.includes('http')
+  const url = isAPI ? API_URL + endpoint : endpoint
+  let headers: { Authorization: string }
 
   if (isAPI) {
-    const { value } = await Preferences.get({ key: "token" });
+    const { value } = await Preferences.get({ key: 'token' })
 
     headers = Object.assign(
       {},
-      !value && value === "null"
+      !value && value === 'null'
         ? null
         : {
             Authorization: `Bearer ${value}`,
           }
-    );
+    )
   }
 
   const options: HttpOptions = {
     url,
     headers,
-  };
+  }
 
-  return await Http.get(options);
-};
+  return await Http.get(options)
+}
 
 export const post = async (endpoint: string, payload: object) => {
-  const isAPI = !endpoint.includes("http");
-  const isChannel = endpoint.includes("broadcasting");
-  const url = isAPI ? API_URL + endpoint : endpoint;
-  const data = keysToSnake(payload);
+  const isAPI = !endpoint.includes('http')
+  const isChannel = endpoint.includes('broadcasting')
+  const url = isAPI ? API_URL + endpoint : endpoint
+  const data = keysToSnake(payload)
   const headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-  };
+    'Content-Type': 'application/x-www-form-urlencoded',
+  }
 
   if (isAPI || isChannel) {
-    const { value } = await Preferences.get({ key: "token" });
+    const { value } = await Preferences.get({ key: 'token' })
 
     Object.assign(
       headers,
-      !value && value === "null"
+      !value && value === 'null'
         ? null
         : {
             Authorization: `Bearer ${value}`,
           }
-    );
+    )
   }
 
   const options: HttpOptions = {
     url,
     data,
     headers,
-  };
+  }
 
-  return await Http.post(options);
-};
+  return await Http.post(options)
+}
 
 export const patch = async (endpoint: string, payload: object) => {
-  const isAPI = !endpoint.includes("http");
-  const url = isAPI ? API_URL + endpoint : endpoint;
-  const data = keysToSnake(payload);
+  const isAPI = !endpoint.includes('http')
+  const url = isAPI ? API_URL + endpoint : endpoint
+  const data = keysToSnake(payload)
   const headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-  };
+    'Content-Type': 'application/x-www-form-urlencoded',
+  }
 
   if (isAPI) {
-    const { value } = await Preferences.get({ key: "token" });
+    const { value } = await Preferences.get({ key: 'token' })
 
     Object.assign(
       headers,
-      !value && value === "null"
+      !value && value === 'null'
         ? null
         : {
             Authorization: `Bearer ${value}`,
           }
-    );
+    )
   }
 
   const options: HttpOptions = {
     url,
     data,
     headers,
-  };
+  }
 
-  return await Http.patch(options);
-};
+  return await Http.patch(options)
+}
